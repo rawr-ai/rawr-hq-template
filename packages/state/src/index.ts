c6af76d06f1e53b842b81ef5f4c4176f29fed493
Matei <tools@matei.work>
2026-02-05 02:21:27 -0500

    feat(state): persist plugin enablement (#13)

---
+++ packages/state/src/index.ts
@@ -0,0 +1,10 @@
+export type { RepoState } from "./types.js";
+export {
+  defaultRepoState,
+  disablePlugin,
+  enablePlugin,
+  getRepoState,
+  setRepoState,
+  statePath,
+} from "./repo-state.js";
+
