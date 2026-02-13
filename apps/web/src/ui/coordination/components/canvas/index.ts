f932365861d24f7903250e3470a611234154aec5
Matei <tools@matei.work>
2026-02-12 20:46:07 -0500

    feat(coordination): make workflowkit editor the visible canvas surface

--- apps/web/src/ui/coordination/components/canvas/index.ts
+++ apps/web/src/ui/coordination/components/canvas/index.ts
@@ -1,6 +1,4 @@
 export { CanvasWorkspace } from "./CanvasWorkspace";
 export { FlowCanvas } from "./FlowCanvas";
-export { FlowEdges } from "./FlowEdges";
-export { FlowNode } from "./FlowNode";
 export { WorkflowSidePanel } from "./WorkflowSidePanel";
 export { WorkflowToolbar } from "./WorkflowToolbar";
