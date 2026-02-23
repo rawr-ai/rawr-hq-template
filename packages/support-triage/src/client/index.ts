de5af0f692a17df0921211289991f47e2d20492b
Matei <tools@matei.work>
2026-02-23 17:26:07 -0500

    refactor(support-triage): inline client router in index

--- packages/support-triage/src/client/index.ts
+++ packages/support-triage/src/client/index.ts
@@ -1,12 +1,12 @@
 import type { RouterClient } from "@orpc/server";
-import { supportTriageClientRouter } from "./router";
-
 export type { SupportTriageClientContext } from "./context";
 export {
   supportTriageClientErrorMap,
   throwSupportTriageDomainErrorAsClientError,
 } from "./errors";
+import { supportTriageClientProcedures } from "./procedures";
+
 export { supportTriageClientProcedures } from "./procedures";
-export { supportTriageClientRouter } from "./router";
+export const supportTriageClientRouter = supportTriageClientProcedures;
 
 export type SupportTriageClient = RouterClient<typeof supportTriageClientRouter>;
